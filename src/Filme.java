70fbfd40c430f2cc3753741a80409d3d1d8b5d7f
Vagnerbs <vagnerb1987@gmail.com>
2025-01-22 23:32:56 -0300

    Definindo ações para o filme

--- src/Filme.java
+++ src/Filme.java
@@ -3,8 +3,21 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
+    double somaDasAvaliacoes;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
 
+    public void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de LAnçamento: " + anoDeLancamento);
+    }
+
+    public void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double pegaMediaDasAvaliacoes(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
 }
